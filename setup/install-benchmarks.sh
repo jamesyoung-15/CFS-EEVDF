cafcca9114392c1130f5e796a391b8f56b1a5b81
James <jyyoung@jyymail.com>
2024-11-12 00:48:49 -0500

    quick fixes

---
+++ setup/install-benchmarks.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+phoronix-test-suite install pts/nginx && phoronix-test-suite install pts/hackbench && phoronix-test-suite install pts/ffmpeg && phoronix-test-suite install pts/compress-7zip && phoronix-test-suite install pts/redis && phoronix-test-suite install pts/c-ray
---
+++ setup/phoronix-run-test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+phoronix-test-suite benchmark pts/nginx pts/hackbench pts/ffmpeg pts/compress-7zip pts/redis pts/c-ray
